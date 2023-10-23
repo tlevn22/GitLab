b0cef68fa40abec089b0f88d9e16d18f110857e2
tlevn22 <tlevinson@g.hmc.edu>
2023-10-23 11:25:30 -0700

    top 10 added

--- process_movie_data.py
+++ process_movie_data.py
@@ -3,11 +3,11 @@
 import csv
 
 
-def find_top_5(filename):
-    """Finds the top 5 highest grossing movies in a CSV dataset.
+def find_top_10(filename):
+    """Finds the top 10 highest grossing movies in a CSV dataset.
        Input: filename, a string - points to filename of dataset
        Output: None
-       Effect: should print five lines of text
+       Effect: should print ten lines of text
     """
     # read in file contents as list of dictionaries
     with open(filename) as f:
@@ -22,10 +22,10 @@ def find_top_5(filename):
     # Sort data and get top 5
     gross_sort = lambda x : x["Gross"]
     rows.sort(key=gross_sort)
-    top_five = rows[:-6:-1]
+    top_ten = rows[:-11:-1]
 
     # Print out results
-    for i, row in enumerate(top_five):
+    for i, row in enumerate(top_ten):
         print("{ind}. {row[Title]} ({row[Year]}) - ${row[Gross]:,d}".format(
             ind=i+1,
             row=row))
@@ -35,4 +35,4 @@ def find_top_5(filename):
 # Movie data comes from "Movie Gross and Ratings" dataset on Kaggle by Yashwanth Sharaf
 # https://www.kaggle.com/datasets/thedevastator/movie-gross-and-ratings-from-1989-to-2014
 if __name__ == "__main__":
-    find_top_5("Movies_gross_rating.csv")
+    find_top_10("Movies_gross_rating.csv")
